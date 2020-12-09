8f19e89bd5d711030b5f476c9a85cdf2ca9da718
amirhoseinghasemi7798 <amirhoseinghasemi7798@gmail.com>
2020-12-09 00:43:35 -0500

    editing file structure

--- src/components/MasterMainPage.js
+++ src/components/MasterMainPage.js
@@ -1,51 +1,66 @@
 import React, { Component } from 'react';
 import { Layout,Menu } from 'antd';
 import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import { ConfigProvider } from 'antd';
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+import ClassList from './ClassList';
+import MasterProfile from './MasterProfile';
+import StudentsList from './StudentsList';
+
 class MasterMainPage extends Component{
     render(){
         const { Header, Content, Footer, Sider } = Layout;
         return(
+          <ConfigProvider direction="rtl">
+          <Router>
             <Layout>
-            <Sider
-              breakpoint="lg"
-              collapsedWidth="0"
-              onBreakpoint={broken => {
-                console.log(broken);
-              }}
-              onCollapse={(collapsed, type) => {
-                console.log(collapsed, type);
-              }}
-            >
-              <div className="logo" />
-              <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-                <Menu.Item key="1" icon={<UserOutlined />}>
-                  nav 1
-                </Menu.Item>
-                <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                  nav 2
-                </Menu.Item>
-                <Menu.Item key="3" icon={<UploadOutlined />}>
-                  nav 3
-                </Menu.Item>
-                <Menu.Item key="4" icon={<UserOutlined />}>
-                  nav 4
-                </Menu.Item>
-              </Menu>
-            </Sider>
-            <Layout>
-              <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-              <Content style={{ margin: '24px 16px 0' }}>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                  content
-                </div>
-              </Content>
-              <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+              <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={broken => {
+                  console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                  console.log(collapsed, type);
+                }}
+              >
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                  
+                  
+                    <Menu.Item key="1" icon={<VideoCameraOutlined />}>
+                      <Link to='/'>کلاس ها</Link>
+                    </Menu.Item>
+                  
+      
+                    <Menu.Item key="2" icon={<UserOutlined />}>
+                      <Link to='/profile'>پروفایل</Link>
+                    </Menu.Item>
+                
+                </Menu>
+              </Sider>
+              <Layout>
+                <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+                <Content style={{ margin: '24px 16px 0' }}>
+                  <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+                    <Route exact path='/' component={ClassList} />
+                    <Route path="/profile" component={MasterProfile} />
+                    <Route path="/students" component={StudentsList} />
+                  </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>All right reserved by Golestan ©2020</Footer>
+              </Layout>
             </Layout>
-          </Layout>
-          
+          </Router>
+        </ConfigProvider>
         );
     }
 }
 
 export default MasterMainPage;
+
+
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,59 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
 import './index.css';
-import { ConfigProvider } from 'antd';
-import { Layout, Menu } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
-import ClassList from './components/ClassList';
-import MasterProfile from './components/MasterProfile';
-import StudentsList from './components/StudentsList';
-
-const { Header, Content, Footer, Sider } = Layout;
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <ConfigProvider direction="rtl">
-    <Router>
-      <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={broken => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-            
-            
-              <Menu.Item key="1" icon={<VideoCameraOutlined />}>
-                <Link to='/'>کلاس ها</Link>
-              </Menu.Item>
-            
-
-              <Menu.Item key="2" icon={<UserOutlined />}>
-                <Link to='/profile'>پروفایل</Link>
-              </Menu.Item>
-          
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              <Route exact path='/' component={ClassList} />
-              <Route path="/profile" component={MasterProfile} />
-              <Route path="/students" component={StudentsList} />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>All right reserved by Golestan ©2020</Footer>
-        </Layout>
-      </Layout>
-    </Router>
-  </ConfigProvider>,
-  document.getElementById('root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
