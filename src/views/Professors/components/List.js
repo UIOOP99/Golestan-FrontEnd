c5d428d2d60217a9fbc2645262153bc6e8a9b8a7
Sajad Hashemian <sajadhsm@cafebazaar.ir>
2020-12-09 15:59:37 +0330

    add professors page

---
+++ src/views/Professors/components/List.js
@@ -0,0 +1,44 @@
+import { Table, Popconfirm, Button } from 'antd';
+
+export default function itemsList({ items, onDelete }) {
+  const columns = [
+    {
+      title: 'شماره شناسایی',
+      dataIndex: 'studentNumber',
+      key: 'studentNumber',
+    },
+    {
+      title: 'نام',
+      dataIndex: 'firstName',
+      key: 'firstName',
+    },
+    {
+      title: 'نام خانوادگی',
+      dataIndex: 'lastName',
+      key: 'lastName',
+    },
+    {
+      title: 'عملیات',
+      key: 'action',
+      render: (text, record) => (
+        <Popconfirm
+          title="آیا مطمئن هستید؟"
+          okText="بله"
+          cancelText="انصراف"
+          onConfirm={() => onDelete(record)}
+        >
+          <Button danger size="small">حذف</Button>
+        </Popconfirm>
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      dataSource={items}
+      rowKey="id"
+      bordered
+    />
+  );
+}
