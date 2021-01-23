c79dd80754acf8928a60e7114b65c79c3cdc3cd0
amirhoseinghasemi7798 <amirhoseinghasemi7798@gmail.com>
2021-01-23 12:51:05 -0500

    RedirectPage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MasterMainPage from './components/MasterMainPage';
-
+import RedirectPage from '../src/RedirectPage'
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     <MasterMainPage />
+     
+      <RedirectPage />
+      
     </div>
   );
 }
---
+++ src/RedirectPage.js
@@ -0,0 +1,38 @@
+import { formatCountdown } from "antd/lib/statistic/utils";
+import React , { Component } from 'react';
+import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+import MasterMainPage from "./components/MasterMainPage";
+class Redirect extends Component{
+
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+
+    render(){
+
+        return(
+        <Router>
+        
+        <div>
+        <Link to='/admin'>پنل ادمین</Link>
+        </div>
+        <div>
+        <Link to='/master'> پنل استاد</Link>
+        </div>
+        <div>
+        <Link to='/student'>پنل دانشجو</Link>
+        </div>
+        
+        <Route path='/master' component={MasterMainPage} />
+
+        </Router>
+        );
+        }
+
+
+}
+
+export default Redirect;
