f073c3adb5d56c86001bf97a142e6d783ad2e64c
Mina22278 <64466790+Mina22278@users.noreply.github.com>
2021-01-24 17:58:19 +0330

    Styling

--- src/views/Login/components/Form.js
+++ src/views/Login/components/Form.js
@@ -1,10 +1,36 @@
 import { Form, Input, Button } from 'antd';
-
+import axios from 'axios';
+import { LockTwoTone,UserOutlined,LoadingOutlined } from '@ant-design/icons';
 function LoginForm() {
+  var role="";
   const onFinish = (values) => {
-    console.log('Success:', values);
-  };
-
+    var apiBaseUrl="";
+  var payLoad={
+    "username":values.username,
+    "password":values.password
+  }
+  axios.post(apiBaseUrl,payLoad).then(function (response) {
+    if(response.data.code == 200){
+    console.log("Login successfull");;
+  axios.get('').then(
+    response=>{
+      this.role=response.data;
+    }
+  );
+  if(role==="STUDENT")
+  //Link;
+ console.log("Student")
+  else if(role==="PROFESSOR")
+ // Link;
+ console.log("Professor")
+  else if( role==="ADMIN")
+  console.log("Admin")
+ // Link;
+    }
+    else console.log("Username password do not match");
+  }
+  );
+};
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
@@ -19,17 +45,17 @@ function LoginForm() {
       <Form.Item
         label="نام کاربری"
         name="username"
-        rules={[{ required: true, message: 'لطفا نام کاربری را وارد کنید.' }]}
+        rules={[{ required: true, pattern:"^[0-9]{9}$",message: 'لطفا نام کاربری را به درستی وارد کنید.' }]}
       >
-        <Input />
+        <Input prefix={<UserOutlined type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username"/>
       </Form.Item>
 
       <Form.Item
         label="رمزعبور"
         name="password"
-        rules={[{ required: true, message: 'لطفا رمز عبور را وارد کنید.' }]}
+        rules={[{ required: true,pattern:"^[0-9]{10}$", message: 'لطفا رمز عبور را به درستی وارد کنید.' }]}
       >
-        <Input.Password />
+        <Input.Password prefix={<LockTwoTone  type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password"  />
       </Form.Item>
 
       <Form.Item >
@@ -40,5 +66,4 @@ function LoginForm() {
     </Form>
   );
 }
-
 export default LoginForm;
