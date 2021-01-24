329c3327017f8674bac540a0b8c3119edbfe53c4
Sajad Hashemian <sajadhsm@gmail.com>
2021-01-24 17:52:18 +0330

    implement base login flow

---
+++ src/shared/services/api/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
+
+export function setAxiosAuthorizationHeader(token) {
+  setInstanceHeader("Authorization", token && `Bearer ${token}`);
+}
+
+function setInstanceHeader(key, value) {
+  if (value) {
+    instance.defaults.headers.common[key] = value;
+  } else {
+    delete instance.defaults.headers.common[key];
+  }
+}
+
+export default instance;
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,13 +1,26 @@
+import { useHistory } from "react-router-dom";
 import { ConfigProvider } from 'antd';
 
 import LoginForm from './components/Form';
 
+const ROLE_ROUTES = {
+  PROFESSOR: '/master',
+  STUDENT: '/student',
+  ADMIN: '/admin',
+};
+
 function LoginPage() {
+  const history = useHistory();
+
+  function redirectionBaseOnRole(role) {
+    history.push(ROLE_ROUTES[role]);
+  }
+
   return (
     <ConfigProvider direction="rtl">
       <h1 style={{ textAlign: 'center' }}>سیستم جامع دانشگاهی گلستان</h1>
       <div style={{ maxWidth: 300, margin: '0 auto' }}>
-        <LoginForm />
+        <LoginForm handleRedirect={redirectionBaseOnRole} />
       </div>
     </ConfigProvider>
   );
