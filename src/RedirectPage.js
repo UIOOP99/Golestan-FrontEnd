a137bd2c6f33c16732fd812f4c8dd9a973fa8bb9
amirhoseinghasemi7798 <amirhoseinghasemi7798@gmail.com>
2021-01-24 02:12:51 -0500

    UserContext added

--- src/RedirectPage.js
+++ src/RedirectPage.js
@@ -1,13 +1,14 @@
 import { formatCountdown } from "antd/lib/statistic/utils";
 import React , { Component } from 'react';
 import { BrowserRouter as Router,Switch,Route,Link } from "react-router-dom";
+import { UserProvider } from "./shared/components/Context/UserContext";
 import MasterMainPage from "./views/Masters/MasterMainPage";
 class Redirect extends Component{
 
     constructor(props){
         super(props);
         this.state={
-
+            JWT_token : '' //set JWT Token here
         }
     }
 
@@ -17,16 +18,20 @@ class Redirect extends Component{
         <Router>
         
         <div>
-        <Link to='/admin'>پنل ادمین</Link>
+            <Link to='/admin'>پنل ادمین</Link>
         </div>
+
         <div>
-        <Link to='/master'> پنل استاد</Link>
-        </div>
-        <div>
-        <Link to='/student'>پنل دانشجو</Link>
+            <Link to='/master'> پنل استاد</Link>
         </div>
         
-        <Route path='/master' component={MasterMainPage} />
+        <div>
+            <Link to='/student'>پنل دانشجو</Link>
+        </div>
+        
+        <UserProvider value={this.state.JWT_token}> 
+            <Route path='/master' component={MasterMainPage} />
+        </UserProvider>
 
         </Router>
         );
