014ba5a36af38a93452adfa339dcafe898d6a869
Sajad Hashemian <sajadhsm@gmail.com>
2021-01-24 18:23:26 +0330

    Revert "Styling"
    This reverts commit f073c3adb5d56c86001bf97a142e6d783ad2e64c.

--- src/views/Login/components/Form.js
+++ src/views/Login/components/Form.js
@@ -1,36 +1,10 @@
 import { Form, Input, Button } from 'antd';
-import axios from 'axios';
-import { LockTwoTone,UserOutlined,LoadingOutlined } from '@ant-design/icons';
+
 function LoginForm() {
-  var role="";
   const onFinish = (values) => {
-    var apiBaseUrl="";
-  var payLoad={
-    "username":values.username,
-    "password":values.password
-  }
-  axios.post(apiBaseUrl,payLoad).then(function (response) {
-    if(response.data.code == 200){
-    console.log("Login successfull");;
-  axios.get('').then(
-    response=>{
-      this.role=response.data;
-    }
-  );
-  if(role==="STUDENT")
-  //Link;
- console.log("Student")
-  else if(role==="PROFESSOR")
- // Link;
- console.log("Professor")
-  else if( role==="ADMIN")
-  console.log("Admin")
- // Link;
-    }
-    else console.log("Username password do not match");
-  }
-  );
-};
+    console.log('Success:', values);
+  };
+
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
   };
@@ -45,17 +19,17 @@ function LoginForm() {
       <Form.Item
         label="نام کاربری"
         name="username"
-        rules={[{ required: true, pattern:"^[0-9]{9}$",message: 'لطفا نام کاربری را به درستی وارد کنید.' }]}
+        rules={[{ required: true, message: 'لطفا نام کاربری را وارد کنید.' }]}
       >
-        <Input prefix={<UserOutlined type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username"/>
+        <Input />
       </Form.Item>
 
       <Form.Item
         label="رمزعبور"
         name="password"
-        rules={[{ required: true,pattern:"^[0-9]{10}$", message: 'لطفا رمز عبور را به درستی وارد کنید.' }]}
+        rules={[{ required: true, message: 'لطفا رمز عبور را وارد کنید.' }]}
       >
-        <Input.Password prefix={<LockTwoTone  type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password"  />
+        <Input.Password />
       </Form.Item>
 
       <Form.Item >
@@ -66,4 +40,5 @@ function LoginForm() {
     </Form>
   );
 }
+
 export default LoginForm;
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,20 +1,13 @@
 import { ConfigProvider } from 'antd';
- import { LockTwoTone,UserOutlined,LoadingOutlined } from '@ant-design/icons';
-import './index1.css';
+
 import LoginForm from './components/Form';
 
 function LoginPage() {
   return (
     <ConfigProvider direction="rtl">
-       <div class="body">
-        <div class="img"><img src="https://www.uniref.ir/images/scientometrics/17.png" width="100px" height="100px" /></div>
-    <div class="main">
-    <p class="b"><span>سیستم جامع دانشگاهی گلستان</span></p>
       <h1 style={{ textAlign: 'center' }}>سیستم جامع دانشگاهی گلستان</h1>
       <div style={{ maxWidth: 300, margin: '0 auto' }}>
         <LoginForm />
-      </div>
-      </div>
       </div>
     </ConfigProvider>
   );
